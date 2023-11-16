a57c8c3a5f9ab4a3031d6fb23a4dabee1ecefb53
Melissa Atkins <classicatkins@gmail.com>
2023-11-15 19:14:04 -0600

    2.3.2

---
+++ Week 2/2.3.2-ComparisonOperatorsAndIfElseStatements.js
@@ -0,0 +1,24 @@
+let age = 25;
+
+// Write an If Else statement
+
+// if age is greater than or equal to 25,
+if (age >= 25) {
+  console.log("Renting a car is more affordable.");
+} else {
+  // if age is less than 25
+  console.log("Renting a car is very expensive.");
+}
+
+// Chaining if statements: if - else if - ... - else
+
+// if age is greater than or equal to 25,
+if (age >= 25) {
+  // if age is less than 25 but greater than or equal to 18
+  console.log("Renting a car is more affordable.");
+} else if (age < 25 && age >= 18) {
+  console.log("Renting a car is very expensive.");
+} else {
+  // if age is less than 18
+  console.log("You cannot legally rent a car.");
+}
